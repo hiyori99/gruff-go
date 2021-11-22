acf8fbef499595d9936f466a4308455f2d022b79
hiyori99 <luluikdkoko@gmail.com>
2021-11-22 17:11:35 +0900

    create example

--- main.go
+++ main.go
@@ -8,9 +8,13 @@ func main() {
     router := gin.Default()
     router.LoadHTMLGlob("templates/*.html")
 
-    router.GET("/", func(ctx *gin.Context){
-        ctx.HTML(200, "index.html", gin.H{})
-    })
+    // router.GET("/", func(ctx *gin.Context){
+    //     ctx.HTML(200, "index.html", gin.H{})
+    // })
+		data := "Hello Go/Gin!!"
+		router.GET("/", func(ctx *gin.Context){	
+    	ctx.HTML(200, "index.html", gin.H{"data": data})
+		})
 
     router.Run()
 }
